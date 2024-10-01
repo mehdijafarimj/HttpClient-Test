be9f207837d523d8590fff6441943d90be459876
mehdijafarimj <mehdij807@gmail.com>
2024-10-01 17:37:18 +0330

    initialize

---
+++ HttpClient_Test/Domain/CreateTaskDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HttpClient_Test.Domain;
+
+public class CreateTaskDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
---
+++ HttpClient_Test/ViewModels/GetAllTaskVm.cs
@@ -0,0 +1,9 @@
+﻿namespace HttpClient_Test.ViewModels;
+
+public class GetAllTaskVm
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreationTime { get; set; }
+}
