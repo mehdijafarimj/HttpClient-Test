c298c121a495483b36cf401e2abac5a2ce69f636
mehdijafarimj <mehdij807@gmail.com>
2024-10-01 19:02:14 +0330

    adding mehtods for all api's in controller

--- HttpClient_Test/Handlers/ToDoListHandler.cs
+++ HttpClient_Test/Handlers/ToDoListHandler.cs
@@ -1,5 +1,61 @@
-﻿namespace HttpClient_Test.Handlers;
+﻿using Newtonsoft.Json;
+using HttpClient_Test.Domain;
+using System.Net;
+
+namespace HttpClient_Test.Handlers;
 
 public class ToDoListHandler
 {
+    private static readonly HttpClient client = new HttpClient()
+    {
+        BaseAddress = new Uri("https://localhost:7057")
+    };
+
+    public string GetAllTasks()
+    {
+        var response = client.GetAsync("api/ToDo").Result;
+
+        var content = response.Content.ReadAsStringAsync().Result;
+        return content;
+    }
+    public bool CreateTask(CreateTaskDto dto)
+    {
+        var response = client.PostAsJsonAsync("api/ToDo",dto).Result;
+
+        if(response.StatusCode != HttpStatusCode.NoContent)
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool Mark(int id)
+    {
+        var response = client.PatchAsJsonAsync($"api/ToDo/mark/{id}",new object()).Result;
+    
+        if(response.StatusCode != HttpStatusCode.NoContent)
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool Unmark(int id)
+    {
+        var response = client.PatchAsJsonAsync($"api/ToDo/unmark/{id}", new object()).Result;
+
+        if(response.StatusCode != HttpStatusCode.NoContent)
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool DeleteTask(int id)
+    {
+        var response = client.DeleteAsync($"api/ToDo/{id}").Result;
+
+        if (response.StatusCode != HttpStatusCode.NoContent)
+        {
+            return false;
+        }
+        return true;
+    }
 }
